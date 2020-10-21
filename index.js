1ad5462eb526c49e344457519d9c05493c4d35dc
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-10-21 12:21:25 +0100

    Move group handlers into src.

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-module.exports = class GroupHandler {
-	constructor(){
-		this.all = []
-		this.mappings = {}
-		this.pairings = []
-		this.unpairings = []
-	}
-
-	/**
-	 * Add a mapping to the internal DB
-	 * @param id
-	 * @param mapping
-	 * @returns {GroupHandler}
-	 */
-	addMapping(id, mapping){
-		if (!Array.isArray(mapping)){
-			mapping = [mapping]
-		}
-
-		this.mappings[id] = mapping
-		for (let map of mapping){
-			if (this.all.indexOf(map) === -1){
-				this.all.push(map)
-			}
-		}
-
-		return this
-	}
+module.exports = {
+	AbstractGroupHandler: require('./src/AbstractGroupHandler')
 }
---
+++ src/AbstractGroupHandler.js
@@ -0,0 +1,29 @@
+module.exports = class AbstractGroupHandler {
+	constructor(){
+		this.all = []
+		this.mappings = {}
+		this.pairings = []
+		this.unpairings = []
+	}
+
+	/**
+	 * Add a mapping to the internal DB
+	 * @param id
+	 * @param mapping
+	 * @returns {AbstractGroupHandler}
+	 */
+	addMapping(id, mapping){
+		if (!Array.isArray(mapping)){
+			mapping = [mapping]
+		}
+
+		this.mappings[id] = mapping
+		for (let map of mapping){
+			if (this.all.indexOf(map) === -1){
+				this.all.push(map)
+			}
+		}
+
+		return this
+	}
+}
