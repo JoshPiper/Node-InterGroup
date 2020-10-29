1b18bacc8c8bef8900518a54963472d32dc28ecc
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-10-29 14:29:14 +0000

    Add discordgrouphandler to handle assigning discord roles.

---
+++ src/DiscordGroupHandler.js
@@ -0,0 +1,26 @@
+let AbstractGroupHandler = require('./AbstractGroupHandler')
+let {Client, GuildMember} = require('discord.js')
+
+module.exports = class DiscordGroupHandler extends AbstractGroupHandler {
+	/**
+	 * @param client Client
+	 * @param guild
+	 */
+	constructor(client, guild){
+		super()
+
+		this.client = client
+		this.guild = this.client.guilds.resolve(client)
+	}
+
+	/**
+	 * @param member GuildMember
+	 * @param groups Array
+	 */
+	handleMember(member, groups){
+		let roles = member.roles.cache.map(role => role.id)
+		let [add, remove] = this.handleGroup(groups, roles)
+
+		console.log(add, remove)
+	}
+}
